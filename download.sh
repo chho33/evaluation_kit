a8590027007794bd7d64f05deb79a12093aebc3f
Nate <jojotenya@gmail.com>
2018-10-25 23:33:38 +0800

    add model

--- download.sh
+++ download.sh
@@ -5,17 +5,19 @@
 
 # download data and models
 data_path="data/"
-model_path="saved_model/"
 dict_fasttext=$data_path"dict_fasttext.txt.gz","1jk6pjs535ujn4SQkK5GTwy_LWaLbyMto"
-sentiment_mapping=$data_path"mapping","1Jl8TURNKydbgw01QuAljOEX4tsHjAcjP"
+sentiment_char_mapping=$data_path"char_mapping","1Jl8TURNKydbgw01QuAljOEX4tsHjAcjP"
+sentiment_word_mapping=$data_path"word_mapping","1lqdsfeBRgc4YHO3go4txu9OKXa7cvjBh"
 lm_mapping=$data_path"words_char.json.gz","1iMD5KAgH4Ro8JqLo_V3XCGLGwz9nsTaw"
-#coh1=$model_path"/coh1/coh2_20181021.tar.gz","1rJUDPJ8nng-vKUNuaSbSGlz1Ye05qOj4"
-coh2_model=$model_path"/coh2/coh2.tar.gz","1rJUDPJ8nng-vKUNuaSbSGlz1Ye05qOj4"
+
+model_path="saved_model/"
+#coh1_model=$model_path"/coh1/coh1.tar.gz","1rJUDPJ8nng-vKUNuaSbSGlz1Ye05qOj4"
+coh2_model=$model_path"coh2/coh2.tar.gz","1rJUDPJ8nng-vKUNuaSbSGlz1Ye05qOj4"
 lm_model=$model_path"LM/LM.tar.gz","16vZJvf5_NqFabcKITOjyVYeAKfryb6Ei"
 sent_model=$model_path"sentiment_analysis/sentiment.zip","1oA9WuYa-jHCimMYRElC7qVuOggrBdaE9"
 
 
-files_arr=($dict_fasttext $sentiment_mapping $lm_mapping)
+files_arr=($dict_fasttext $sentiment_char_mapping $sentiment_word_mapping $lm_mapping $coh2_model $lm_model $sent_model)
 for f in ${files_arr[@]}; do
     IFS=',' read filename fileid <<< "${f}"
     curl -L -o "${filename}" "https://drive.google.com/uc?export=download&id=${fileid}"
@@ -44,9 +46,8 @@ data_path="data/"
 fasttext_model=$PWD/$data_path"cc.zh.300.bin"
 dict_fasttext=$PWD/$data_path"dict_fasttext.txt"
 test_raw=$PWD/$data_path"test_raw.csv"
-echo $test_raw
-sentiment_mapping=$PWD/$data_path"mapping"
-echo $sentiment_mapping
+sentiment_char_mapping=$PWD/$data_path"char_mapping"
+sentiment_word_mapping=$PWD/$data_path"word_mapping"
 lm_mapping=$PWD/$data_path"words_char.json"
 ln -s $dict_fasttext coh1/;
 ln -s $test_raw coh1/corpus/;
@@ -57,4 +58,5 @@ ln -s $dict_fasttext LM/data/;
 ln -s $test_raw LM/data/;
 ln -s $lm_mapping LM/data/;
 ln -s $test_raw sentiment_analysis/corpus/;
-ln -s $sentiment_mapping sentiment_analysis/corpus/;
+ln -s $sentiment_char_mapping sentiment_analysis/corpus/;
+ln -s $sentiment_word_mapping sentiment_analysis/corpus/;
--- run.sh
+++ run.sh
@@ -7,13 +7,15 @@ lm_model_path=$model_base_path/LM
 coh1_model_path=$model_base_path/coh1
 coh2_model_path=$model_base_path/coh2
 sent_model_path=$model_base_path/sentiment_analysis
+sent_char_mapping=$PWD/$data_path"char_mapping"
+sent_word_mapping=$PWD/$data_path"word_mapping"
 
 # get LM score
-cd LM; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path --model_path=$lm_model_path; cd ..
+cd LM; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path --model_dir=$lm_model_path; cd ..
 # get sentiment score
-cd sentiment_analysis; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path --model_path=coh1_model_path; cd ..
+cd sentiment_analysis; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path --model_dir=$sent_model_path --jieba_dict=$data_path/dict_fasttext.txt --mapping_path=$sent_word_mapping --model_type=rnn_last --sentence_cut_mode=word --vocab_size=50000; cd ..
 # get coh2 score
-cd coh2; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path  --model_path=coh2_model_path; cd ..
+cd coh2; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path  --model_dir=$coh2_model_path; cd ..
 # get coh1 score
-cd coh1; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path --model_path=coh1_model_path; cd ..
+cd coh1; python infer_scores.py --inference_data_path=$data_path --log_path=$log_path --model_dir=$coh1_model_path; cd ..
 echo "------------------" >> $log_path
