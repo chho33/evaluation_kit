13b9311247358135c9e0b4bb4134f2b5479d5933
Nate <jojotenya@gmail.com>
2018-10-30 09:23:35 +0800

    add sent char

--- download.sh
+++ download.sh
@@ -6,7 +6,7 @@
 # download data and models
 data_path="data/"
 dict_fasttext=$data_path"dict_fasttext.txt.gz","1jk6pjs535ujn4SQkK5GTwy_LWaLbyMto"
-sentiment_char_mapping=$data_path"char_mapping","1Jl8TURNKydbgw01QuAljOEX4tsHjAcjP"
+sentiment_char_mapping=$data_path"char_mapping","1CenvR6GH8qaSn2evOA3_T6rSn0EUDfUN"
 sentiment_word_mapping=$data_path"word_mapping","1lqdsfeBRgc4YHO3go4txu9OKXa7cvjBh"
 lm_mapping=$data_path"words_char.json.gz","1iMD5KAgH4Ro8JqLo_V3XCGLGwz9nsTaw"
 
@@ -14,10 +14,11 @@ model_path="saved_model/"
 coh1_model=$model_path"/coh1/coh1.tar.gz","1FkVBItYo4ra9B-yF76o3h3WCQ9lLSs7p"
 coh2_model=$model_path"coh2/coh2.tar.gz","1rJUDPJ8nng-vKUNuaSbSGlz1Ye05qOj4"
 lm_model=$model_path"LM/LM.tar.gz","16vZJvf5_NqFabcKITOjyVYeAKfryb6Ei"
-sent_model=$model_path"sentiment_analysis/sentiment.zip","1oA9WuYa-jHCimMYRElC7qVuOggrBdaE9"
+sent_word_model=$model_path"sentiment_analysis/sentiment_word.zip","1oA9WuYa-jHCimMYRElC7qVuOggrBdaE9"
+sent_char_model=$model_path"sentiment_analysis/sentiment_char.zip","1TqLDuN2YO9ddJH5HDaY1oVk3xl__CdlN"
 
 
-files_arr=($dict_fasttext $sentiment_char_mapping $sentiment_word_mapping $lm_mapping $coh1_model $coh2_model $lm_model $sent_model)
+files_arr=($dict_fasttext $sentiment_char_mapping $sentiment_word_mapping $lm_mapping $coh1_model $coh2_model $lm_model $sent_word_model $sent_char_model)
 for f in ${files_arr[@]}; do
     IFS=',' read filename fileid <<< "${f}"
     curl -L -o "${filename}" "https://drive.google.com/uc?export=download&id=${fileid}"
