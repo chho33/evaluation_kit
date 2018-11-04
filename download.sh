c7490053ddebad3c8fd4344887cd955142e789ed
Nate <jojotenya@gmail.com>
2018-11-04 20:41:13 +0800

    add data for coh1

--- download.sh
+++ download.sh
@@ -5,6 +5,9 @@
 
 # download data and models
 data_path="data/"
+coh1_source_mapping=$data_path"source.150000.mapping","1I1SiWYmtvSQSmI-JiXoqcb1az1GjeKzl"
+coh1_target_mapping=$data_path"target.6185.mapping","16Q-GAJ-JwZRLJKZiWKbgcVvcaLJH8p3Z"
+fasttext_npy=$data_path"fasttext.npy.zip","1p3ZpcBeZcpIjMmbx3aD0QWEks-TtrPSP"
 dict_fasttext=$data_path"dict_fasttext.txt.gz","1jk6pjs535ujn4SQkK5GTwy_LWaLbyMto"
 sentiment_char_mapping=$data_path"char_mapping","1CenvR6GH8qaSn2evOA3_T6rSn0EUDfUN"
 sentiment_word_mapping=$data_path"word_mapping","1lqdsfeBRgc4YHO3go4txu9OKXa7cvjBh"
@@ -18,7 +21,7 @@ sent_word_model=$model_path"sentiment_analysis/sentiment_word.zip","1oA9WuYa-jHC
 sent_char_model=$model_path"sentiment_analysis/sentiment_char.tar.gz","112GPe7_tIoqKQwcgiXBgh6FeFPn7-ZK8"
 
 
-files_arr=($dict_fasttext $sentiment_char_mapping $sentiment_word_mapping $lm_mapping $coh1_model $coh2_model $lm_model $sent_word_model $sent_char_model)
+files_arr=($coh1_source_mapping $coh1_target_mapping $fasttext_npy $dict_fasttext $sentiment_char_mapping $sentiment_word_mapping $lm_mapping $coh1_model $coh2_model $lm_model $sent_word_model $sent_char_model)
 for f in ${files_arr[@]}; do
     IFS=',' read filename fileid <<< "${f}"
     download_url="https://drive.google.com/uc?id=${fileid}&export=download"
@@ -50,11 +53,17 @@ data_path="data/"
 fasttext_model=$PWD/$data_path"cc.zh.300.bin"
 dict_fasttext=$PWD/$data_path"dict_fasttext.txt"
 test_raw=$PWD/$data_path"test_raw.csv"
+coh1_source_mapping=$PWD/$data_path"source.150000.mapping"
+coh1_target_mapping=$PWD/$data_path"target.6185.mapping"
+fasttext_npy=$PWD/$data_path"fasttext.npy"
 sentiment_char_mapping=$PWD/$data_path"char_mapping"
 sentiment_word_mapping=$PWD/$data_path"word_mapping"
 lm_mapping=$PWD/$data_path"words_char.json"
 ln -s $dict_fasttext coh1/;
 ln -s $test_raw coh1/corpus/;
+ln -s $coh1_source_mapping coh1/corpus/;
+ln -s $coh1_target_mapping coh1/corpus/;
+ln -s $fasttext_npy coh1/corpus/;
 ln -s $dict_fasttext coh2/data/;
 ln -s $fasttext_model coh2/data/;
 ln -s $test_raw coh2/data/;
